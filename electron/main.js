05f685ca733bc5f0cc6683e1457c461ad9ecdbb1
Burhanuddin Udaipurwala <udaipurwalaburhanuddin@gmail.com>
2020-07-19 09:22:22 +0530

    fixed an issue with crash on startup

--- electron/main.js
+++ electron/main.js
@@ -1,39 +1,45 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
-const url = require('url');
-const { channels } = require('../src/shared/constants');
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+const url = require("url");
+const { channels } = require("../src/shared/constants");
 
 let mainWindow;
 
-function createWindow () {
-  const startUrl = process.env.ELECTRON_START_URL || url.format({
-    pathname: path.join(__dirname, '../index.html'),
-    protocol: 'file:',
-    slashes: true,
+function createWindow() {
+  const startUrl =
+    process.env.ELECTRON_START_URL ||
+    url.format({
+      pathname: path.join(__dirname, "../index.html"),
+      protocol: "file:",
+      slashes: true,
+    });
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: { nodeIntegration: true },
   });
-  mainWindow = new BrowserWindow({ width: 800, height: 600 });
   mainWindow.loadURL(startUrl);
-  mainWindow.on('closed', function () {
+  mainWindow.on("closed", function() {
     mainWindow = null;
   });
 }
 
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", function() {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', function () {
+app.on("activate", function() {
   if (mainWindow === null) {
     createWindow();
   }
 });
 
 ipcMain.on(channels.APP_INFO, (event) => {
-  event.sender.send(channels.APP_INFO, { 
+  event.sender.send(channels.APP_INFO, {
     appName: app.getName(),
     appVersion: app.getVersion(),
   });
